9399af3a07feb16b3d8b50844442222de5a34590
Mateusz Krakowiak <mateusz.krakowiak@onet.pl>
2020-04-06 08:33:32 +0200

    Remove previous implementation

--- src/roulette/slackbot/urls.py
+++ src/roulette/slackbot/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 app_name = 'slackbot'
 urlpatterns = [
-    path('configuration', views.configuration, name='configuration'),
-    path('send_message', views.send_message, name='send_message'),
-    path('message_sent', views.message_sent, name='message_sent'),
 ]
--- src/roulette/slackbot/views.py
+++ src/roulette/slackbot/views.py
@@ -1,18 +1,1 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.decorators.http import require_POST
-from django.urls import reverse
 
-from django_slack import slack_message
-
-
-def configuration(request):
-    return render(request, 'slackbot/configuration.html', {})
-
-@require_POST
-def send_message(request):
-    slack_message('slackbot/test_message.slack', {})
-    return HttpResponseRedirect(reverse('slackbot:message_sent'))
-
-def message_sent(request):
-    return render(request, 'slackbot/message_sent.html')
